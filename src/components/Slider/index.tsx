68476bb774ebb0f35e31d10333764458c77a193c
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-02-12 09:37:14 -0300

    refactor: remove slider

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,30 +1,9 @@
-import 'react-awesome-slider/dist/styles.css'
-import AutoplayHoc from 'react-awesome-slider/dist/autoplay'
-import { Container, AwesomeSliderStyled } from './styles'
+import { Container } from './styles'
 
 export const Slider = (): JSX.Element => {
-  const AutoplaySlider = AutoplayHoc(AwesomeSliderStyled)
-
-  const data: Array<{ source: string; slug: string }> = [
-    { source: '/slider/desktop/01.png', slug: 'this-is-zero' },
-    { source: '/slider/desktop/02.png', slug: 'this-is-one' },
-    { source: '/slider/desktop/03.png', slug: 'this-is-two' }
-  ]
-
   return (
     <Container>
-      <AutoplaySlider
-        play
-        name="images-slider"
-        startup={true}
-        activityColor={'green'}
-        animation="scaleOutAnimation"
-      >
-        {data.length > 0 &&
-          data.map(({ source, slug }) => (
-            <div key={source} data-slug={slug} data-src={source} />
-          ))}
-      </AutoplaySlider>
+      <img src="/slider/desktop/background.png" />
     </Container>
   )
 }
